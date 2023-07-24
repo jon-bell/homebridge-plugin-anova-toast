e449f5cae59ba700e4afa01328ec889e121cb282
Jonathan Bell <jon@jonbell.net>
2023-07-23 20:31:58 -0400

    WIP for v2

--- src/AnovaOvenService.ts
+++ src/AnovaOvenService.ts
@@ -1,60 +1,60 @@
 import axios from 'axios';
 import EventEmitter from 'events';
-import pino from 'pino';
 import TypedEventEmitter from 'typed-emitter';
 import { v4 as uuidv4 } from 'uuid';
 import WebSocket from 'ws';
 import {
-  AnovaOvenResponse, DeviceID, OvenCommand, OvenCommandRequest, OvenCommandResponse, OvenStateMessage,
-  OvenStateResponse, StagesEntity, StartCookPayload, SteamGenerators, TemperatureBulbSetPoint,
+  AnovaOvenResponse, DeviceID, OvenCommand, OvenCommandResponse, OvenResponse, OvenStateMessage,
+  StagesEntity, StartCookPayload, SteamGenerators, TemperatureBulbSetPoint,
 } from './AnovaTypes';
+import { Logger } from 'homebridge';
 
-const logger = pino({ level: 'info' });
-
-const steamGeneratorsEqual = (a: SteamGenerators | undefined, b: SteamGenerators | undefined) =>{
-  if(!a && !b) {
+const steamGeneratorsEqual = (a: SteamGenerators | undefined, b: SteamGenerators | undefined) => {
+  if (!a && !b) {
     return true;
   }
-  if(!a || !b){
+  if (!a || !b) {
     return false;
   }
-  if(a.mode !== b.mode){
+  if (a.mode !== b.mode) {
     return false;
   }
-  if(a.mode === 'steam-percentage'){
+  if (a.mode === 'steam-percentage') {
     return a.steamPercentage?.setpoint === b.steamPercentage?.setpoint;
-  }else{
+  } else {
     throw `Unknown steam mode ${a.mode}`;
   }
 };
 const temperatureBulbsEqual = (a: TemperatureBulbSetPoint, b: TemperatureBulbSetPoint) => {
-  if(a.mode !== b.mode){
+  if (a.mode !== b.mode) {
     return false;
   }
-  if(a.mode === 'dry' && b.mode === 'dry'){
+  if (a.mode === 'dry' && b.mode === 'dry') {
     return a.dry.setpoint.celsius === b.dry.setpoint.celsius;
-  } else{
+  } else {
     throw `Unknown temp bulb mode ${a.mode}`;
   }
 };
-const stagesEqual = (stage1: StagesEntity, stage2: StagesEntity)=>{
+const stagesEqual = (stage1: StagesEntity, stage2: StagesEntity) => {
   return stage1.fan.speed === stage2.fan.speed &&
-  stage1.heatingElements.bottom.on === stage2.heatingElements.bottom.on&&
-  stage1.heatingElements.rear.on === stage2.heatingElements.rear.on &&
-  stage1.heatingElements.top.on === stage2.heatingElements.top.on &&
-  steamGeneratorsEqual(stage1.steamGenerators, stage2.steamGenerators) &&
-  temperatureBulbsEqual(stage1.temperatureBulbs, stage2.temperatureBulbs) &&
-  stage1.vent.open === stage2.vent.open;
+    stage1.heatingElements.bottom.on === stage2.heatingElements.bottom.on &&
+    stage1.heatingElements.rear.on === stage2.heatingElements.rear.on &&
+    stage1.heatingElements.top.on === stage2.heatingElements.top.on &&
+    steamGeneratorsEqual(stage1.steamGenerators, stage2.steamGenerators) &&
+    temperatureBulbsEqual(stage1.temperatureBulbs, stage2.temperatureBulbs) &&
+    stage1.vent.open === stage2.vent.open;
 };
 
 export class AnovaOven extends (EventEmitter as new () => TypedEventEmitter<AnovaOvenResponse>) {
   private _id: DeviceID;
+  private _name: string;
   private _curStateMessage: OvenStateMessage;
   private _service: AnovaOvenService;
 
-  public constructor(id: DeviceID, startingState: OvenStateMessage, service: AnovaOvenService) {
+  public constructor(id: DeviceID, name: string, startingState: OvenStateMessage, service: AnovaOvenService) {
     super();
     this._id = id;
+    this._name = name;
     this.on('ovenState', (update) => {
       this._curStateMessage = update;
     });
@@ -62,12 +62,15 @@ export class AnovaOven extends (EventEmitter as new () => TypedEventEmitter<Anov
     this._service = service;
   }
 
+  public get name() {
+    return this._name;
+  }
+
   public set curStateMessage(update: OvenStateMessage) {
     this.emit('ovenState', update);
     if (update.state.mode !== this._curStateMessage.state.mode) {
-      logger.info(`Oven ${this._id} is in state ${update.state.mode}`);
+      this._service.log.info(`Oven ${this._id} is in state ${update.state.mode}`);
     }
-    logger.trace(JSON.stringify(update, null, 2));
     this._curStateMessage = update;
   }
 
@@ -83,46 +86,20 @@ export class AnovaOven extends (EventEmitter as new () => TypedEventEmitter<Anov
     return this._curStateMessage;
   }
 
-  private async _sendOvenCommand(command: OvenCommand) {
-    const cmd: OvenCommandRequest = {
-      command: 'SEND_OVEN_COMMAND',
-      payload: {
-        deviceId: this._id,
-        requestId: uuidv4(),
-        command: command,
-      },
-    };
-    logger.info(`Sending command ${cmd.payload.requestId}`);
-    return new Promise<void>((resolve, reject) => {
-      const timeout = setTimeout(() => {
-        reject(`Timeout waiting for response to command ${cmd.payload.requestId}`);
-      }, 5000);
 
-      const watchdog = (response) => {
-        if (response.requestId === cmd.payload.requestId) {
-          this._service.off('commandResponse', watchdog);
-          clearTimeout(timeout);
-
-          if (response.success) {
-            resolve();
-          } else {
-            logger.error(`Command ${cmd.payload.requestId} failed:`);
-            logger.error(JSON.stringify(response, null, 2));
-            reject(`Command ${cmd.payload.requestId} failed`);
-          }
-        }
-      };
-      this._service.on('commandResponse', watchdog);
-      this._service.send(cmd);
+  private async _startCook(payload: StartCookPayload) {
+    await this._service.sendOvenCommand('CMD_APO_START', {
+      payload: payload,
+      type: 'CMD_APO_START',
+      id: this._id,
     });
   }
 
-  private async _startCook(payload: StartCookPayload) {
-    await this._sendOvenCommand({ id: uuidv4(), type: 'startCook', payload: payload });
-  }
-
   public async stopCook() {
-    await this._sendOvenCommand({ id: uuidv4(), type: 'stopCook' });
+    await this._service.sendOvenCommand('CMD_APO_STOP', {
+      type: 'CMD_APO_STOP',
+      id: this._id,
+    });
   }
 
   /**
@@ -132,14 +109,14 @@ export class AnovaOven extends (EventEmitter as new () => TypedEventEmitter<Anov
    *
    * @param stages
    */
-  public async startCook(stages: StagesEntity[]){
+  public async startCook(stages: StagesEntity[]) {
     stages.forEach(eachStage => {
-      if(!eachStage.id) {
+      if (!eachStage.id) {
         eachStage.id = uuidv4();
       }
     });
     const cookId = uuidv4();
-    await this._startCook({cookId, stages});
+    await this._startCook({ cookId, stages });
     return cookId;
   }
 
@@ -147,12 +124,12 @@ export class AnovaOven extends (EventEmitter as new () => TypedEventEmitter<Anov
    * Returns true if the currently active cook has the same stages (by object equality, does not rely on stage ID or cook ID)
    * @param stages
    */
-  public isCooking(stages: StagesEntity[]){
+  public isCooking(stages: StagesEntity[]) {
     const curCook = this._curStateMessage.cook;
-    if(!curCook){
+    if (!curCook) {
       return false;
     }
-    if(curCook.stages.length !== stages.length){
+    if (curCook.stages.length !== stages.length) {
       return false;
     }
     return stages.every((v, idx) => stagesEqual(v, curCook.stages[idx]));
@@ -296,11 +273,13 @@ export default class AnovaOvenService extends (EventEmitter as new () => TypedEv
   private _email: string;
   private _password: string;
   private _socket: WebSocket;
+  public log: Logger;
   public readonly ovens: Map<DeviceID, AnovaOven> = new Map();
 
-  public constructor(args: { email: string; password: string }) {
+  public constructor(args: { email: string; password: string }, logger: Logger) {
     super();
     this._email = args.email;
+    this.log = logger;
     this._password = args.password;
   }
 
@@ -320,22 +299,20 @@ export default class AnovaOvenService extends (EventEmitter as new () => TypedEv
 
     return new Promise((resolve, reject) => {
       //open socket to API
-      this._socket = new WebSocket('wss://app.oven.anovaculinary.io');
+      this._socket = new WebSocket(`wss://devices.anovaculinary.io/?token=${data.idToken}&supportedAccessories=APO&platform=android`,
+        'ANOVA_V2');
 
-      //once opened, authenticate
+
+      let authenticated = false;
       this._socket.on('message', (data) => {
-        const message = JSON.parse(data.toString());
-        if (message.response === 'AUTH_TOKEN_RESPONSE') {
+        const message = JSON.parse(data.toString()) as OvenResponse;
+        if (message.command === 'EVENT_APO_WIFI_LIST' && !authenticated) {
+          authenticated = true;
           this.emit('authenticated');
-        } else {
-          this._dispatchToOven(message);
         }
+        this._dispatchToOven(message);
       });
 
-      this._socket.on('open', () => {
-        logger.info('socket open');
-        this._socket.send(JSON.stringify({ command: 'AUTH_TOKEN', payload: data.idToken }));
-      });
       const timeout = setTimeout(() => {
         reject('Timeout connecting to API or authentication failed');
       }, 15000);
@@ -347,33 +324,59 @@ export default class AnovaOvenService extends (EventEmitter as new () => TypedEv
     );
   }
 
-  private _dispatchToOven(message: { response: string; ovenId?: DeviceID; deviceId?: DeviceID }) {
-    const ovenId = message.ovenId || message.deviceId;
-    if (!ovenId) {
-      logger.error('Message without ovenId received');
-      logger.error(JSON.stringify(message, null, 2));
-      return;
-    }
-    logger.info(`Message for oven ${ovenId} ${message.response}`);
-    if (message.response === 'OVEN_STATE') {
-      const stateMessage = message as OvenStateResponse;
+  private _pendingOvens: { cookerId: DeviceID; name: string }[] = [];
+  private _dispatchToOven(message: OvenResponse) {
+    if (message.command === 'EVENT_APO_WIFI_LIST') {
+      message.payload.forEach((oven) => {
+        this._pendingOvens.push({ cookerId: oven.cookerId, name: oven.name });
+      });
+    } else if (message.command === 'EVENT_APO_STATE') {
+      const ovenId = message.payload.cookerId;
+      this.log.info(`Oven ${ovenId} state changed`);
       let oven = this.ovens.get(ovenId);
       if (!oven) {
-        oven = new AnovaOven(ovenId, stateMessage.data, this);
+        const ovenName = this._pendingOvens.find((eachOven) => eachOven.cookerId === ovenId)?.name;
+        oven = new AnovaOven(ovenId, ovenName || `Oven ${ovenId.substring(0, 4)}`, message.payload.state, this);
         this.ovens.set(ovenId, oven);
         this.emit('ovenFound', oven);
       }
-      oven.curStateMessage = stateMessage.data;
-    } else if (message.response === 'OVEN_COMMAND_RESPONSE') {
-      this.emit('commandResponse', message as OvenCommandResponse);
+      oven.curStateMessage = message.payload.state;
+    } else if (message.command === 'RESPONSE') {
+      this.emit('commandResponse', message);
     } else {
-      logger.debug('Unknown message type received');
-      logger.debug(JSON.stringify(message, null, 2));
+      this.log.warn(`Unknown message type received: ${(message as { command: string }).command}`);
+      this.log.debug(JSON.stringify(message, null, 2));
     }
   }
 
-  public send(message: OvenCommandRequest): void {
-    this._socket.send(JSON.stringify(message));
-  }
+  public async sendOvenCommand<T extends OvenCommand>(command: T['command'], payload: T['payload']) {
+    const cmd = {
+      command,
+      payload,
+      requestId: uuidv4(),
+    };
+    this.log.info(`Sending command ${cmd.requestId}`);
+    return new Promise<void>((resolve, reject) => {
+      const timeout = setTimeout(() => {
+        reject(`Timeout waiting for response to command ${cmd.requestId}`);
+      }, 5000);
 
+      const watchdog = (response: OvenCommandResponse) => {
+        if (response.requestId === cmd.requestId) {
+          this.off('commandResponse', watchdog);
+          clearTimeout(timeout);
+
+          if (response.payload.status === 'ok') {
+            resolve();
+          } else {
+            this.log.error(`Command ${cmd.requestId} failed:`);
+            this.log.error(JSON.stringify(response, null, 2));
+            reject(`Command ${cmd.requestId} failed`);
+          }
+        }
+      };
+      this.on('commandResponse', watchdog);
+      this._socket.send(JSON.stringify(cmd));
+    });
+  }
 }
